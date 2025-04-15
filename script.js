fa8f04c82a0ece730a7a74ce327c5f831d63e8de
Ajay <mailtoajay7007@gmail.com>
2025-04-15 22:42:08 +0530

    custom foreach

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // getting original elements of arrays
-Array.prototype.newElement=4
+// Array.prototype.newElement=4
 const originalArray = [1, 2, 3, 4, 5];
+
+// originalArray.forEach(i=> console.log(i**2))
 
 // for (i in originalArray) {
 //     if(originalArray.hasOwnProperty(i))
@@ -14,3 +16,42 @@ const originalArray = [1, 2, 3, 4, 5];
    
 //         console.log(originalArray[i]);
 // }
+
+
+//custom for each
+
+// 1.
+// const customForEach = (array, callback) => {
+//     for(let i = 0; i < array.length; i++) {
+//         callback(array[i]);
+//     }
+//     return array;
+// }
+
+// customForEach(originalArray, (element) => {
+//     console.log(element**2);
+// })
+
+
+
+
+// 2.
+Array.prototype.customForEach = function(callback) {
+    for (let i=0;i<this.length;i++){
+        callback(this[i],i,this)
+    }
+    return this;
+}
+
+originalArray.customForEach((element) => {
+    console.log(element**2);
+})
+
+// 3.
+// const customForEacht = function(callback) {
+//     for (let i=0;i<this.length;i++){
+//         callback(this[i],i,this)
+//     }
+//     return this;
+// }
+// Array.prototype.customForEach = customForEacht;
