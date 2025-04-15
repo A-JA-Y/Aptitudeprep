339f5bf3c190cde5ff36a04b014a262f291b9f77
Ajay <mailtoajay7007@gmail.com>
2025-04-15 22:19:17 +0530

    original elements of array

---
+++ script.js
@@ -0,0 +1,16 @@
+// getting original elements of arrays
+Array.prototype.newElement=4
+const originalArray = [1, 2, 3, 4, 5];
+
+// for (i in originalArray) {
+//     if(originalArray.hasOwnProperty(i))
+//         {
+//             console.log(i);
+//         }
+   
+//     }
+
+// for(let i = 0; i < originalArray.length; i++) {
+   
+//         console.log(originalArray[i]);
+// }
